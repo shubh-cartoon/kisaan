95f2fefdbe69343bad5fc7963ac8c95de1f7b546
shubh-cartoon <shubhamgadiya7374@gmail.com>
2020-01-20 10:31:02 +0530

    CRUD Done

--- app/controllers/farmer_instruments_controller.rb
+++ app/controllers/farmer_instruments_controller.rb
@@ -19,6 +19,21 @@ class FarmerInstrumentsController < ApplicationController
 		redirect_to farmer_instrument_path(@advertise)
 	end
 
+	def edit
+		@farmer_instrument=FarmerInstrument.find(params[:id])
+	end
+
+	def update
+		@farmer_instrument_rec=FarmerInstrument.find(params[:id])
+		@farmer_instrument_rec.update(rent_per_hour: params[:farmer_instrument][:rent_per_hour], deposit: params[:farmer_instrument][:deposit], is_available: params[:farmer_instrument][:is_available], available_from: params[:farmer_instrument][:available_from], available_to: params[:farmer_instrument][:available_to])
+		redirect_to farmer_instrument_path(@farmer_instrument_rec)
+	end
+
+	def destroy
+		@delete_record=FarmerInstrument.find(params[:id]).destroy
+		redirect_to farmer_instruments_path
+	end
+
 	private
 	def permit_params
 		params[:farmer_instrument].permit(:farmer_id, :instrument_id, :rent_per_hour, :deposit, :available_from, :available_to, :is_available)
--- app/models/farmer_instrument.rb
+++ app/models/farmer_instrument.rb
@@ -5,4 +5,11 @@ class FarmerInstrument < ApplicationRecord
 	has_many :rents
 	has_many :borrowers, through: :rents, class_name: "Farmer", foreign_key: 'farmer_id'
 
+	def farmer_name
+		farmer.full_name
+	end
+	def instrument_name
+		instrument.name
+	end
+
 end
